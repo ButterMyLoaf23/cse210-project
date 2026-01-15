d62c738d5b2b33ce70674aced1ee6e5edc925b8d
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-01-14 18:13:15 -0700

    Learn 02 Completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1.jobTitle = "Software Engineering";
+        job1.company = "Microsoft";
+        job1.startYear = 2019;
+        job1.endYear = 2022;
+
+        Job job2 = new Job();
+        job2.jobTitle = "Manager";
+        job2.company = "Apple";
+        job2.startYear = 2022;
+        job2.endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume.name = "Allison Rose";
+
+        myResume.jobs.Add(job1);
+        myResume.jobs.Add(job2);
+
+        myResume.Display();
     }
 }
