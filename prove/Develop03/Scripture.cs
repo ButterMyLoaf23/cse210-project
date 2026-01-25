9faeb98246c656cda92b1cfe356d4b1b55a61a2e
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-01-24 20:02:27 -0700

    Develop 3 completed

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+    private Random _random;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _random = new Random();
+        _words = text.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(words => new Word(words)).ToList();
+    }
+
+    public string GetDisplayText()
+    {
+        string referenceText = _reference.GetDisplayText();
+        string scriptureText = string.Join(" ", _words.Select(words => words.GetDisplayText()));
+        return $"{referenceText} {scriptureText}";
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        List<Word> visibleWords = _words.Where(words => !words.IsHidden()).ToList();
+
+        if (visibleWords.Count == 0)
+        {
+            return;
+        }
+
+        int countToHide = Math.Min(numberToHide, visibleWords.Count);
+
+        for (int i = 0; i < countToHide; i++)
+        {
+            int index = _random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(words => words.IsHidden());
+    }
+}
+
