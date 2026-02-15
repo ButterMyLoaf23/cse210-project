d3cf276db8632941d90afca4615ceb9211c2edef
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-02-14 21:10:39 -0700

    Program finished

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address1 = new Address("123 Main St", "Phoenix", "AZ", "USA");
+        Customer customer1 = new Customer("Boston Wyatt", address1);
+
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Culinary knife", "KN001", 20.99, 1));
+        order1.AddProduct(new Product("Monster Energy Drink", "MN039", 3.58, 1));
+
+        Console.WriteLine("Order 1:");
+        Console.WriteLine("PackingLabel:");
+        Console.WriteLine(order1.PackingLabel());
+        Console.WriteLine();
+
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order1.ShippingLabel());
+        Console.WriteLine();
+
+        Console.WriteLine($"Total Cost: ${order1.TotalCost()}");
+        Console.WriteLine();
+
+
+        Address address2 = new Address("456 W Queen St", "Toronto", "ON", "Canada");
+        Customer customer2 = new Customer("Ryan Reynolds", address2);
+
+        Order order2 = new Order(customer2);
+
+        order1.AddProduct(new Product("Monitor", "M129", 129.99, 1));
+        order1.AddProduct(new Product("HDMI Cable", "CBL01", 10.00, 2));
+
+        Console.WriteLine("Order 2:");
+        Console.WriteLine("PackingLabe2:");
+        Console.WriteLine(order2.PackingLabel());
+        Console.WriteLine();
+
+        Console.WriteLine("Shipping Label:");
+        Console.WriteLine(order2.ShippingLabel());
+        Console.WriteLine();
+
+        Console.WriteLine($"Total Cost: ${order2.TotalCost()}");
+        Console.WriteLine();
+    
     }
 }
