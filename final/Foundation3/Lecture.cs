0ec5ef1a47facf9e00b31d7a764d324b253b71c6
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-02-20 19:26:49 -0700

    Lecture class created

---
+++ final/Foundation3/Lecture.cs
@@ -0,0 +1,24 @@
+using System.Net.Quic;
+
+public class Lecture : Event
+{
+    private string _speaker;
+    private int _capacity;
+
+    public Lecture(string title, string description, string date, string time, string address, string speaker, int capacity) : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;   
+    }
+
+    public override string GetFullDetails()
+    {
+        return GetStandardDetails() + $"Type: Lecture \n Speaker: {_speaker} \n Capacity: {_capacity}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Lecture: {_speaker} - {GetShortDescription()}";
+    }
+
+}
