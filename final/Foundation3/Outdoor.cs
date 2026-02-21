44e9cdfaf70c4245a5803b52bf0f3a813c9261c1
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-02-20 19:37:28 -0700

    Outdoor class created

---
+++ final/Foundation3/Outdoor.cs
@@ -0,0 +1,19 @@
+public class Outdoor : Event
+{
+    private string _weather;
+
+    public Outdoor(string title, string description, string date, string time, string address, string weather) : base(title, description, date, time, address)
+    {
+        _weather = weather;
+    }
+
+    public override string GetFullDetails()
+    {
+        return GetStandardDetails() + $"\n Type: Outdoor Gathering \n Weather: {_weather}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Outdoor Event - {GetShortDescription()}";
+    }
+}
